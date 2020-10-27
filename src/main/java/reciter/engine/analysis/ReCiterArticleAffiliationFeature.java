aeb38b31f7f9f7859434c680eec1811070e0e339
Sarbajit Dutta <sarbajitdutta35@gmail.com>
2020-10-27 11:17:53 -0400

    add affiliations to authors

--- src/main/java/reciter/engine/analysis/ReCiterArticleAffiliationFeature.java
+++ src/main/java/reciter/engine/analysis/ReCiterArticleAffiliationFeature.java
@@ -1,15 +1,55 @@
 package reciter.engine.analysis;
 
+import java.util.List;
+
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTyped;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperFieldModel.DynamoDBAttributeType;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import reciter.engine.analysis.evidence.AffiliationEvidence.InstitutionalAffiliationSource;
 
+/**
+ * This class contains affiliation statements from multiple sources
+ */
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @DynamoDBDocument
 public class ReCiterArticleAffiliationFeature {
-    private String affiliationPubmed;
-    private String affiliationScopus;
-    private String affiliationScopusId;
+    /**
+     * Affiliation Statement from Pubmed
+     */
+    private String affiliationStatementLabel;
+    /**
+     * Affiliation Source (PUBMED)
+     */
+    @DynamoDBTyped(DynamoDBAttributeType.S)
+    private InstitutionalAffiliationSource affiliationStatementLabelSource;
+    /**
+     * List of affiliations for the author
+     */
+    private List<ReCiterArticleAffiliationInstitution> affiliationInstitutions;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @DynamoDBDocument
+    public class ReCiterArticleAffiliationInstitution {
+        /**
+         * Affiliation Label from SCOPUS
+         */
+        private String affiliationInstitutionLabel;
+        /**
+         * Affiliation IDs
+         */
+        private Integer affiliationInstitutionId;
+        /**
+         * Affiliation Source (SCOPUS)
+         */
+        @DynamoDBTyped(DynamoDBAttributeType.S)
+        private InstitutionalAffiliationSource affiliationInstitutionSource;
+    }
 }
